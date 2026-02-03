824d37178c087bb88af9bf65c45dfa8da23805e6
Lino Meyer <lino.meyer@stud.hslu.ch>
2026-02-03 15:40:08 +0100

    finish tech radar

--- javascript-clientside/exercises/technology-radar/index.js
+++ javascript-clientside/exercises/technology-radar/index.js
@@ -1,32 +1,53 @@
-const container = document.querySelector('.technologies');
+const form = document.getElementById('add-tech-form');
+form.addEventListener('submit', (event) => addTechnology(event));
 
 async function postTechnology(technology) {
-  const response = await fetch(
-    'https://65c1f652f7e6ea59682a27c8.mockapi.io/Technologies',
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(technology),
-    },
-  );
-  return response.json();
+    const response = await fetch(
+        'https://65c1f652f7e6ea59682a27c8.mockapi.io/Technologies',
+        {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(technology),
+        },
+    );
+    return response.json();
 }
 
-const form = document.getElementById('add-tech-form');
 
 async function addTechnology(event) {
-  event.preventDefault();
+    event.preventDefault();
+    const data = new FormData(form);
 
-  const data = new FormData(form);
+    const technology = {}
 
-  const technology = {
-    name: data['name'],
-    category: data['category'],
-    ring: data['ring'],
-    description: data['description'],
-  };
+    for (let [key, value] of data.entries()) {
+        technology[key] = value
+    }
+    const result = await postTechnology(technology);
 
-  const result = await postTechnology(technology);
+    if (result) {
+        await renderTechnologies()
+    }
 }
 
-form.addEventListener('submit', (event) => addTechnology(event));
+async function getTechnologies() {
+    const response = await fetch(
+        'https://65c1f652f7e6ea59682a27c8.mockapi.io/Technologies',
+    );
+    return response.json();
+}
+
+async function renderTechnologies() {
+    const technologies = await this.getTechnologies();
+    technologies.forEach((technology) => {
+        this.innerHTML +=
+            `    <div>\n
+                <article>\n
+                    <header class="name">${technology.name}</header>\n
+                    <p class="category">${technology.category}</p>\n
+                    <p class="ring">${technology.ring}</p>\n
+                    <p class="description">${technology.description}</p>\n
+                </article>\n
+            </div>`;
+    });
+}
